e4757d31aa5771372d8e2a06078c1d7a0c808f0e
gregj77 <greg_j@live.com>
2014-11-04 15:17:04 +0100

    Source code for the .net threading labs.

---
+++ Lab5Threading/ThreadingClass/ThreadingClass/DataHelper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingClass
+{
+    public static class DataHelper
+    {
+        public static string LoadData()
+        {
+            using (StreamReader rdr = new StreamReader("input.txt"))
+            {
+                return rdr.ReadToEnd();
+            }
+        }
+
+        public static ICollection<Func<int>> FindStringCountActions(string input, string searchTerm, long[] timeTaken, int? concurrencyOverride)
+        {
+            List<Func<int>> functions = new List<Func<int>>();
+            Stopwatch sw = new Stopwatch();
+
+            int totalParts = concurrencyOverride.GetValueOrDefault(Environment.ProcessorCount);
+            int firstTaskStarting = 0;
+            int lastTaskCompleting = 0;
+
+            foreach (var part in DataHelper.DivideInputString(input, totalParts))
+            {
+                int startIdx = part.Item1;
+                int len = part.Item2;
+
+                Func<int> workDelegate = () =>
+                    {
+                        if (Interlocked.Increment(ref firstTaskStarting) == 1)
+                        {
+                            sw.Start();
+                        }
+
+                        int result = FindStringCount(input, startIdx, len, searchTerm);
+
+                        if (Interlocked.Increment(ref lastTaskCompleting) == totalParts)
+                        {
+                            sw.Stop();
+                            timeTaken[0] = sw.ElapsedMilliseconds;
+                        }
+                        return result;
+                    };
+
+                functions.Add(workDelegate);
+            }
+
+            return functions;
+        }
+
+        private static int FindStringCount(string input, int startIndex, int len, string searchTerm)
+        {
+            int foundCount = 0;
+            char[] items = searchTerm.ToCharArray();
+            int charsToSearch = searchTerm.Length;
+            int findIndex = 0;
+
+            for (int i = startIndex; i < startIndex + len + searchTerm.Length - 1 && i < input.Length; ++i)
+            {
+                char current = input[i];
+                if (items[findIndex] == current)
+                {
+                    ++findIndex;
+                    if (findIndex == charsToSearch)
+                    {
+                        ++foundCount;
+                        findIndex = 0;
+                    }
+                }
+                else
+                {
+                    findIndex = 0;
+                }
+                if (i % 100 == 0)
+                {
+                    Thread.SpinWait(50000);
+                }
+            }
+            return foundCount;
+        }
+
+        private static IEnumerable<Tuple<int, int>> DivideInputString(string input, int concurrencyLevel = 1)
+        {
+            if (1 == concurrencyLevel)
+            {
+                yield return Tuple.Create(0, input.Length);
+                yield break;
+            }
+
+            int partSize = input.Length / concurrencyLevel;
+            int start = 0;
+
+            for (int i = 0; i < concurrencyLevel; ++i)
+            {
+                yield return Tuple.Create(start, partSize);
+                start += partSize;
+            }
+        }
+    }
+}
---
+++ Lab5Threading/ThreadingClass/ThreadingClass/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+
+namespace ThreadingClass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly string _data;
+        private readonly string _searchTerm = "abc";
+        private readonly SynchronizationContext _ctx = SynchronizationContext.Current;
+
+        private readonly long[] _processingTime = new long[1];
+        private int _result = 0;
+        private int _level = Environment.ProcessorCount;
+
+        public int ConcurrencyLevel
+        {
+            get { return _level; }
+            set
+            {
+                _level = value;
+                if (null != PropertyChanged)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ConcurrencyLevel"));
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _data = DataHelper.LoadData();
+        }
+
+        private void ResetTimeAndResult(object sender)
+        {
+            ((Button)sender).IsEnabled = false;
+            ((Button)sender).Content = "running....";
+            _processingTime[0] = 0;
+            _result = 0;
+        }
+
+        private void WriteResult(Label target, object sender)
+        {
+            _ctx.Post(_ =>
+                {
+                    ((Button)sender).IsEnabled = true;
+                    ((Button)sender).Content = "Run";
+
+                    target.Content = string.Format("Found {0} occurences of '{1}' in {2} ms", _result, _searchTerm, _processingTime[0]);
+                }, null);
+        }
+
+        private void btnSingleThread_Click(object sender, RoutedEventArgs e)
+        {
+            ResetTimeAndResult(sender);
+
+            foreach (var func in DataHelper.FindStringCountActions(_data, _searchTerm, _processingTime, ConcurrencyLevel))
+            {
+                _result += func();
+            }
+            WriteResult(Res1, sender);
+        }
+
+        private void btnNewThread_Click(object sender, RoutedEventArgs e)
+        {
+            ResetTimeAndResult(sender);
+            var functions = DataHelper.FindStringCountActions(_data, _searchTerm, _processingTime, ConcurrencyLevel);
+            int scheduled = functions.Count;
+
+            foreach (var func in functions)
+            {
+                new Thread(_ =>
+                    {
+                        Interlocked.Add(ref _result, func());
+                        if (Interlocked.Decrement(ref scheduled) == 0)
+                            WriteResult(Res2, sender);
+                    })
+                    .Start();
+            }
+            
+        }
+
+        private void btnThredPool_Click(object sender, RoutedEventArgs e)
+        {
+            ResetTimeAndResult(sender);
+            var functions = DataHelper.FindStringCountActions(_data, _searchTerm, _processingTime, ConcurrencyLevel);
+            int scheduled = functions.Count;
+
+            foreach (var func in functions)
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                    {
+                    Interlocked.Add(ref _result, func());
+                    if (Interlocked.Decrement(ref scheduled) == 0)
+                        WriteResult(Res3, sender);
+                    }, null);
+            }
+        }
+
+        private void btnTask_Click(object sender, RoutedEventArgs e)
+        {
+            ResetTimeAndResult(sender);
+            var functions = DataHelper.FindStringCountActions(_data, _searchTerm, _processingTime, ConcurrencyLevel);
+            int scheduled = functions.Count;
+
+            List<Task<int>> tasks = new List<Task<int>>();
+            foreach(var func in functions)
+            {
+                tasks.Add(Task.Factory.StartNew(func)); 
+            }
+
+            Task.Factory.ContinueWhenAll(tasks.ToArray(), completed => 
+                {
+                    _result = completed.Sum(t => t.Result);
+                    WriteResult(Res4, sender);
+                });
+        }
+
+        private void btnClassicApm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+   
+    }
+}
