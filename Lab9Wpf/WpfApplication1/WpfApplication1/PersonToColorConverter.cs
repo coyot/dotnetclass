af45edbf581323e4b8974d6037c57ecdb46df8f7
Maciej Paszta <maciej.paszta@gmail.com>
2014-12-10 20:03:22 +0100

    Laboratoria z WPF

---
+++ Lab9Wpf/WpfApplication1/WpfApplication1/PersonToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApplication1
+{
+    public class PersonToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Person p = value as Person;
+            if (p == null)
+            {
+                return new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            }
+            if (p.Name.StartsWith("A"))
+                return new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            return new SolidColorBrush(Color.FromRgb(0, 0, 255));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab9Wpf/WpfApplication1/WpfApplication1/PersonViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+
+namespace WpfApplication1
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public PersonViewModel()
+        {         
+            People = new ObservableCollection<Person>();
+            DodajCommand = new DelegateCommand(Dodaj, CzyDodaj);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DelegateCommand DodajCommand { get; private set; }
+
+        private void Dodaj()
+        {
+            var person = new Person
+            {
+                Name = Name,
+                Surname = Surname
+            };
+            People.Add(person);
+        }
+
+        private bool CzyDodaj()
+        {
+            return !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Surname);
+        }
+
+        public ObservableCollection<Person> People { get; set; } 
+
+        private string _name;
+        public string Name
+        {
+            get { return _name;}
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+                DodajCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _surname;
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                _surname = value;
+                OnPropertyChanged();
+                DodajCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string name = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
